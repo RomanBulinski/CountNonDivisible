dc02e167113f6b10e3776df9126637fc20578f16
Roman Bulinski <aboandrus@gmail.com>
2019-03-23 12:29:24 +0100

    Implemented code

--- src/main/java/CountNonDivisible.java
+++ src/main/java/CountNonDivisible.java
@@ -1,5 +1,20 @@
+
 public class CountNonDivisible {
 
+
+    public int[] solution(int[] A){
+        int[] result = new int[A.length];
+        for (int i = 0; i<A.length; i++){
+            int counter =0;
+            for (int j = 0; j<A.length; j++){
+                if(A[i]/A[j]!=0 ){
+                    counter++;
+                }
+            }
+            result[i]=counter;
+        }
+        return result;
+    }
 
 
 
